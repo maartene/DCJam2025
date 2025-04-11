2f3cbe23c8ab9fe36cb38aa6c879779169518239
Maarten Engels <maartene@mac.com>
2025-04-11 20:33:33 +0200

    First pass visualizing the map

--- Sources/DCJam2025/main.swift
+++ Sources/DCJam2025/main.swift
@@ -3,27 +3,50 @@ import Raylib
 let screenWidth: Int32 = 800
 let screenHeight: Int32 = 450
 
-Raylib.initWindow(screenWidth, screenHeight, "MyGame")
+Raylib.initWindow(screenWidth, screenHeight, "DCJam2025")
 Raylib.setTargetFPS(30)
-let randomColors: [Color] = [.blue, .red, .green, .yellow, .darkBlue, .maroon, .magenta]
-var ballColor: Color = .maroon
-var ballPosition = Vector2(x: -100, y: -100)
-var previousBallPosition: Vector2
+
+let world = World(map: Map([
+    ["#","#","#","#","#","#"],
+    ["#",".",".",".","#","#"],
+    ["#",".","#",".",".","#"],
+    ["#",".","#","#",".","#"],
+    ["#",".",".",".",".","#"],
+    ["#","#","#","#","#","#"],
+]), partyStartPosition: Coordinate(x: 1, y: 1))
+
+var camera = Camera3D()
+camera.projection = .perspective
+camera.fovy = 60
+camera.up = Vector3(x: 0, y: 1, z: 0)
+camera.position = Vector3(x: 1, y: 0, z: 1)
+camera.target = Vector3(x: 2, y: 0, z: 1)
+
 while Raylib.windowShouldClose == false {
     // update
-    previousBallPosition = ballPosition
-    ballPosition = Raylib.getMousePosition()
-    if Raylib.isMouseButtonDown(.left) {
-        ballColor = randomColors.randomElement() ?? .black
-    }
-    let size = max(abs(ballPosition.x - previousBallPosition.x) + abs(ballPosition.y - previousBallPosition.y), 10)
+    Raylib.updateCamera(&camera)
 
     // draw
     Raylib.beginDrawing()
-    Raylib.clearBackground(.rayWhite)
-    Raylib.drawText("Hello, world!", 425, 25, 25, .darkGray)
-    Raylib.drawCircleV(ballPosition, size, ballColor)
+    Raylib.clearBackground(.darkGray)
+    Raylib.beginMode3D(camera)
+        for row in world.map.minY ... world.map.maxY {
+            for column in world.map.minX ... world.map.maxX {
+                let coordinate = Coordinate(x: column, y: row)
+                if world.map.tileAt(coordinate) == .wall {
+                    Raylib.drawCubeV(coordinate.toVector3, .one, .blue)
+                }
+            }
+        }
+    Raylib.endMode3D()
+    
     Raylib.drawFPS(10, 10)
     Raylib.endDrawing()
 }
 Raylib.closeWindow()
+
+extension Coordinate {
+    var toVector3: Vector3 {
+        Vector3(x: Float(x), y: 0, z: Float(y))
+    }
+}
