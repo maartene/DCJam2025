5fac64533924edbf3d4fda8a056db8acdc549e5c
Maarten Engels <maartene@mac.com>
2025-04-11 21:07:57 +0200

    R: move Game class into own file

--- Sources/DCJam2025/Presentation/Conversions.swift
+++ Sources/DCJam2025/Presentation/Conversions.swift
@@ -5,7 +5,25 @@
 //  Created by Maarten Engels on 11/04/2025.
 //
 
+import Raylib
+
 func light(position: Coordinate, vantagePoint: Coordinate) -> Float {
     let distance = position.distanceTo(vantagePoint)
     return 1.0 / Float(distance)
+}
+
+extension Coordinate {
+    var toVector3: Vector3 {
+        Vector3(x: Float(x), y: 0, z: Float(y))
+    }
+}
+
+extension Color {
+    static func *(color: Color, scalar: Float) -> Color {
+        let r = Float(color.r) * scalar
+        let g = Float(color.g) * scalar
+        let b = Float(color.b) * scalar
+        
+        return Color(r: UInt8(r), g: UInt8(g), b: UInt8(b), a: color.a)
+    }
 }
