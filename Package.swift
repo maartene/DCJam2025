c78ba3ba20c5fd2325404371ab996485670420cd
Maarten Engels <maartene@mac.com>
2025-04-11 20:47:04 +0200

    R: place all bootstrapping in Game class so its no longer necessary to use @MainActor

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "DCJam2025",
+    platforms: [
+        .macOS(.v15)
+    ],
     dependencies: [
         .package(url: "https://github.com/STREGAsGate/Raylib.git", branch: "master")
     ],
--- Sources/DCJam2025/main.swift
+++ Sources/DCJam2025/main.swift
@@ -1,65 +1,70 @@
 import Raylib
 
-let screenWidth: Int32 = 800
-let screenHeight: Int32 = 450
+class Game {
+    let screenWidth: Int32 = 800
+    let screenHeight: Int32 = 450
+    var camera = makeCamera()
+    
+    let world = World(map: Map([
+        ["#","#","#","#","#","#"],
+        ["#",".",".",".","#","#"],
+        ["#",".","#",".",".","#"],
+        ["#",".","#","#",".","#"],
+        ["#",".",".",".",".","#"],
+        ["#","#","#","#","#","#"],
+    ]), partyStartPosition: Coordinate(x: 1, y: 1))
+    
+    func run() {
+        Raylib.initWindow(screenWidth, screenHeight, "DCJam2025")
+        Raylib.setTargetFPS(30)
 
-Raylib.initWindow(screenWidth, screenHeight, "DCJam2025")
-Raylib.setTargetFPS(30)
+        while Raylib.windowShouldClose == false {
+            update()
+            drawGameView()
+        }
+        Raylib.closeWindow()
+    }
+    
+    private static func makeCamera() -> Camera3D {
+        var camera = Camera3D()
+        camera.projection = .perspective
+        camera.fovy = 60
+        camera.up = Vector3(x: 0, y: 1, z: 0)
+        camera.position = Vector3(x: 1, y: 0, z: 1)
+        camera.target = Vector3(x: 2, y: 0, z: 1)
+        return camera
+    }
+    
+    private func update() {
+        Raylib.updateCamera(&camera)
+    }
 
-let world = World(map: Map([
-    ["#","#","#","#","#","#"],
-    ["#",".",".",".","#","#"],
-    ["#",".","#",".",".","#"],
-    ["#",".","#","#",".","#"],
-    ["#",".",".",".",".","#"],
-    ["#","#","#","#","#","#"],
-]), partyStartPosition: Coordinate(x: 1, y: 1))
+    private func drawGameView() {
+        Raylib.beginDrawing()
+            Raylib.clearBackground(.darkGray)
+            draw3D()
+            Raylib.drawFPS(10, 10)
+        Raylib.endDrawing()
+    }
 
-var camera = makeCamera()
-
-while Raylib.windowShouldClose == false {
-    update()
-    drawGameView()
+    private func draw3D() {
+        Raylib.beginMode3D(camera)
+            for row in world.map.minY ... world.map.maxY {
+                for column in world.map.minX ... world.map.maxX {
+                    let coordinate = Coordinate(x: column, y: row)
+                    if world.map.tileAt(coordinate) == .wall {
+                        Raylib.drawCubeV(coordinate.toVector3, .one, .blue)
+                    }
+                }
+            }
+        Raylib.endMode3D()
+    }
 }
-Raylib.closeWindow()
+
+Game().run()
 
 extension Coordinate {
     var toVector3: Vector3 {
         Vector3(x: Float(x), y: 0, z: Float(y))
     }
-}
-
-private func makeCamera() -> Camera3D {
-    var camera = Camera3D()
-    camera.projection = .perspective
-    camera.fovy = 60
-    camera.up = Vector3(x: 0, y: 1, z: 0)
-    camera.position = Vector3(x: 1, y: 0, z: 1)
-    camera.target = Vector3(x: 2, y: 0, z: 1)
-    return camera
-}
-
-@MainActor private func update() {
-    Raylib.updateCamera(&camera)
-}
-
-@MainActor private func drawGameView() {
-    Raylib.beginDrawing()
-        Raylib.clearBackground(.darkGray)
-        draw3D()
-        Raylib.drawFPS(10, 10)
-    Raylib.endDrawing()
-}
-
-@MainActor private func draw3D() {
-    Raylib.beginMode3D(camera)
-        for row in world.map.minY ... world.map.maxY {
-            for column in world.map.minX ... world.map.maxX {
-                let coordinate = Coordinate(x: column, y: row)
-                if world.map.tileAt(coordinate) == .wall {
-                    Raylib.drawCubeV(coordinate.toVector3, .one, .blue)
-                }
-            }
-        }
-    Raylib.endMode3D()
 }
