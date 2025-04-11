769ef92c49530df6a2d4993e8d2cbc4c3e5886da
Maarten Engels <maartene@mac.com>
2025-04-11 20:38:36 +0200

    R: extract functions in main

--- Sources/DCJam2025/main.swift
+++ Sources/DCJam2025/main.swift
@@ -15,20 +15,43 @@ let world = World(map: Map([
     ["#","#","#","#","#","#"],
 ]), partyStartPosition: Coordinate(x: 1, y: 1))
 
-var camera = Camera3D()
-camera.projection = .perspective
-camera.fovy = 60
-camera.up = Vector3(x: 0, y: 1, z: 0)
-camera.position = Vector3(x: 1, y: 0, z: 1)
-camera.target = Vector3(x: 2, y: 0, z: 1)
+var camera = makeCamera()
 
 while Raylib.windowShouldClose == false {
-    // update
-    Raylib.updateCamera(&camera)
+    update()
+    drawGameView()
+}
+Raylib.closeWindow()
 
-    // draw
+extension Coordinate {
+    var toVector3: Vector3 {
+        Vector3(x: Float(x), y: 0, z: Float(y))
+    }
+}
+
+private func makeCamera() -> Camera3D {
+    var camera = Camera3D()
+    camera.projection = .perspective
+    camera.fovy = 60
+    camera.up = Vector3(x: 0, y: 1, z: 0)
+    camera.position = Vector3(x: 1, y: 0, z: 1)
+    camera.target = Vector3(x: 2, y: 0, z: 1)
+    return camera
+}
+
+@MainActor private func update() {
+    Raylib.updateCamera(&camera)
+}
+
+@MainActor private func drawGameView() {
     Raylib.beginDrawing()
-    Raylib.clearBackground(.darkGray)
+        Raylib.clearBackground(.darkGray)
+        draw3D()
+        Raylib.drawFPS(10, 10)
+    Raylib.endDrawing()
+}
+
+@MainActor private func draw3D() {
     Raylib.beginMode3D(camera)
         for row in world.map.minY ... world.map.maxY {
             for column in world.map.minX ... world.map.maxX {
@@ -39,14 +62,4 @@ while Raylib.windowShouldClose == false {
             }
         }
     Raylib.endMode3D()
-    
-    Raylib.drawFPS(10, 10)
-    Raylib.endDrawing()
-}
-Raylib.closeWindow()
-
-extension Coordinate {
-    var toVector3: Vector3 {
-        Vector3(x: Float(x), y: 0, z: Float(y))
-    }
 }
