d2c7b9096f0f4567eafebf0246dea6bc9ae423b5
Maarten Engels <maartene@thedreamweb.eu>
2025-08-10 00:01:20 +0200

    R: remove unneeded tests for light

--- Tests/DCJam2025Tests/VizualizationTests.swift
+++ Tests/DCJam2025Tests/VizualizationTests.swift
@@ -65,36 +65,6 @@ import Model
         #expect(target(from: testcase.position, heading: testcase.heading) == testcase.expectedTargetPosition)
     }
 
-    @Test("Calculate the correct amount of light of a coordinate, when viewed from a specific coordinate", arguments: [
-        (Coordinate(x: 10, y: 10), Coordinate(x: -3, y: 4), Float(0.069843024)),
-        (Coordinate(x: 4, y: 0), Coordinate(x: 3, y: 0), Float(1.0)),
-        (Coordinate(x: -1, y: 8), Coordinate(x: -3, y: 0), Float(0.12126782)),
-        (Coordinate(x: -9, y: -9), Coordinate(x: -2, y: -3), Float(0.10846523)),
-        (Coordinate(x: 7, y: 2), Coordinate(x: 4, y: 4), Float(0.2773501)),
-        (Coordinate(x: -10, y: 10), Coordinate(x: 0, y: -4), Float(0.05812382)),
-        (Coordinate(x: -8, y: 8), Coordinate(x: 4, y: 4), Float(0.07905694)),
-        (Coordinate(x: 9, y: -3), Coordinate(x: -1, y: 4), Float(0.081923194)),
-        (Coordinate(x: 1, y: -10), Coordinate(x: 4, y: 1), Float(0.0877058)),
-        (Coordinate(x: 4, y: -3), Coordinate(x: 2, y: -1), Float(0.35355338)),
-        (Coordinate(x: 8, y: 7), Coordinate(x: -1, y: -2), Float(0.078567415)),
-        (Coordinate(x: -10, y: 7), Coordinate(x: 0, y: 3), Float(0.09284767)),
-        (Coordinate(x: 5, y: 4), Coordinate(x: 2, y: -2), Float(0.1490712)),
-        (Coordinate(x: -9, y: -10), Coordinate(x: -2, y: 0), Float(0.081923194)),
-        (Coordinate(x: -8, y: 8), Coordinate(x: 3, y: -3), Float(0.06428244)),
-        (Coordinate(x: -10, y: 10), Coordinate(x: 3, y: -1), Float(0.058722023)),
-        (Coordinate(x: 0, y: 0), Coordinate(x: -3, y: 2), Float(0.2773501)),
-        (Coordinate(x: -5, y: 5), Coordinate(x: 1, y: -3), Float(0.1)),
-        (Coordinate(x: -1, y: 1), Coordinate(x: 4, y: 3), Float(0.18569534)),
-        (Coordinate(x: 7, y: 8), Coordinate(x: 2, y: 3), Float(0.14142136))
-    ]) func testLightCalculation(testcase: (position: Coordinate, vantagePoint: Coordinate, expectedLight: Float)) {
-        #expect(light(position: testcase.position, vantagePoint: testcase.vantagePoint) == testcase.expectedLight)
-    }
-
-    @Test("Maximize the amount of light to 1 if vantage point and coordiante are the same") func maxLightCalculation() {
-        let position = Coordinate(x: 10, y: 15)
-        #expect(light(position: position, vantagePoint: position) == 1)
-    }
-
     @Test("Calculate the multiple of a color and a scalar", arguments: [
         (Color(r: 255, g: 255, b: 255, a: 255), Float(0.5), Color(r: 127, g: 127, b: 127, a: 255)),
         (Color(r: 113, g: 194, b: 157, a: 255), Float(0.8371812), Color(r: 94, g: 162, b: 131, a: 255)),
