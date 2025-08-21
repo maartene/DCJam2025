028b847ff37cfa5f22d6286d24fd28b6e15420a4
Maarten Engels <maartene@thedreamweb.eu>
2025-08-21 16:08:50 +0200

    R: extract world and ability creation from PartyMemberAbility tests

--- Tests/DCJam2025Tests/PartyMemberAbility.swift
+++ Tests/DCJam2025Tests/PartyMemberAbility.swift
@@ -5,14 +5,18 @@ import Model
 
 
 @Suite("The party should be able to perform abilities") struct PartyPerfomsAbilitiesTests {
-    @Suite("given a generic ability") struct GenericAbility {
-        @Test("should be able to be executed") func executeAbility() {
-            let world = World(floors: [Floor()])
-            var count = 0
-            let ability = SpyAbility {
-                count += 1
+    @Suite("given a generic ability") class GenericAbility {
+        let world = World(floors: [Floor()])
+        private(set) var count = 0
+        private(set) var ability: any Ability = MockAbility(properties: [:])
+        
+        init() {
+            ability = SpyAbility {
+                self.count += 1
             }
-            
+        }
+        
+        @Test("should be able to be executed") func executeAbility() {
             world.executeCommand(.executeAbility(user: .frontRight, ability: ability), at: Date())
             
             #expect(count == 1)
@@ -20,12 +24,6 @@ import Model
         }
         
         @Test("should not be executed when ability is in cooldown") func abilityInCooldown() {
-            let world = World(floors: [Floor()])
-            var count = 0
-            let ability = SpyAbility {
-                count += 1
-            }
-            
             world.executeCommand(.executeAbility(user: .frontRight, ability: ability), at: Date())
             world.executeCommand(.executeAbility(user: .frontRight, ability: ability), at: Date())
             
@@ -33,12 +31,6 @@ import Model
         }
         
         @Test("should not be executed when party member is KOd") func abilityWhenMemberIsKo() {
-            let world = World(floors: [Floor()])
-            var count = 0
-            let ability = SpyAbility {
-                count += 1
-            }
-            
             world.partyMembers[.frontLeft].takeDamage(Int.max)
             world.executeCommand(.executeAbility(user: .frontLeft, ability: ability), at: Date())
             
